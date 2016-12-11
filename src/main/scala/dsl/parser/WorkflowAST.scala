c0fc1f2fe8c0e94a88143d9c92dc3e8640578d2b
kirankumarbs <kiranprofessional2@gmail.com>
2016-12-12 01:57:03 +0530

    Updated

--- src/main/scala/dsl/parser/WorkflowAST.scala
+++ src/main/scala/dsl/parser/WorkflowAST.scala
@@ -7,8 +7,8 @@ sealed trait WorkflowAST
 case class AndThen(step1: WorkflowAST, step2: WorkflowAST) extends WorkflowAST
 case class ReadInput(inputs: Seq[String]) extends WorkflowAST
 case class CallService(serviceName: String) extends WorkflowAST
-case class choice(alternatives: Seq[ConditionThen]) extends WorkflowAST
-case object EXIT extends WorkflowAST
+case class Choice(alternatives: Seq[ConditionThen]) extends WorkflowAST
+case object Exit extends WorkflowAST
 
 sealed trait ConditionThen { def thenBlock: WorkflowAST }
 case class IfThen(predicate: Condition, thenBlock: WorkflowAST) extends ConditionThen
--- src/main/scala/dsl/parser/WorkflowParser.scala
+++ src/main/scala/dsl/parser/WorkflowParser.scala
@@ -1,6 +1,6 @@
 package dsl.parser
 
-import dsl.token.{IDENTIFIER, WorkflowToken}
+import dsl.token._
 
 import scala.util.parsing.combinator.Parsers
 import scala.util.parsing.input.{NoPosition, Position, Reader}
@@ -14,6 +14,49 @@ object WorkflowParser extends Parsers{
 
   private def identifier: Parser[IDENTIFIER] = {
     accept("identifier", { case id @ IDENTIFIER(name) => id})
+  }
+
+  private def literal: Parser[LITERAL] = {
+    accept("string literal", { case lit @ LITERAL(name) => lit})
+  }
+
+  def condition: Parser[Equals] = {
+    (identifier ~ EQUALS ~ literal) ^^ { case id ~ eq ~ lit => Equals(id.str, lit.str) }
+  }
+
+
+  def program: Parser[WorkflowAST] = {
+    phrase(block)
+  }
+
+  def block: Parser[WorkflowAST] = {
+    rep1(statement) ^^ { case stmtList => stmtList reduceRight AndThen }
+  }
+
+  def statement: Parser[WorkflowAST] = {
+    val exit = EXIT ^^ (_ => Exit)
+    val readInput = READINPUT ~ rep(identifier ~ COMMA) ~ identifier ^^ {
+      case read ~ inputs ~ IDENTIFIER(lastInput) => ReadInput(inputs.map(_._1.str) ++ List(lastInput))
+    }
+    val callService = CALLSERVICE ~ literal ^^ {
+      case call ~ LITERAL(serviceName) => CallService(serviceName)
+    }
+    val switch = SWITCH ~ COLON ~ INDENT ~ rep1(ifThen) ~ opt(otherwiseThen) ~ DEDENT ^^ {
+      case _ ~ _ ~ _ ~ ifs ~ otherwise ~ _ => Choice(ifs ++ otherwise)
+    }
+    exit | readInput | callService | switch
+  }
+
+  def ifThen: Parser[IfThen] = {
+    (condition ~ ARROW ~ INDENT ~ block ~ DEDENT) ^^ {
+      case cond ~ _ ~ _ ~ block ~ _ => IfThen(cond, block)
+    }
+  }
+
+  def otherwiseThen: Parser[OtherwiseThen] = {
+    (OTHERWISE ~ ARROW ~ INDENT ~ block ~ DEDENT) ^^ {
+      case _ ~ _ ~ _ ~ block ~ _ => OtherwiseThen(block)
+    }
   }
 
 }
